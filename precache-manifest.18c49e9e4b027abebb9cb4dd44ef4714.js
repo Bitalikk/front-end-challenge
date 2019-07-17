33932da34343fd25091a1f8c5818e95d0a7ccc1a
Vitalii Chenchovyi <orgvitaliy@gmail.com>
2019-07-17 17:16:49 +0300

    Updates

---
+++ precache-manifest.18c49e9e4b027abebb9cb4dd44ef4714.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1934a03f4341016d63e9a5f9cafe8548",
+    "url": "/front-end-challenge/index.html"
+  },
+  {
+    "revision": "1a4c9487d20dbe98c164",
+    "url": "/front-end-challenge/static/css/main.113d25c7.chunk.css"
+  },
+  {
+    "revision": "84f533f5d16725afc6e7",
+    "url": "/front-end-challenge/static/js/2.c09f8263.chunk.js"
+  },
+  {
+    "revision": "1a4c9487d20dbe98c164",
+    "url": "/front-end-challenge/static/js/main.b91e9fa0.chunk.js"
+  },
+  {
+    "revision": "8b89d11e7bef3bb347e5",
+    "url": "/front-end-challenge/static/js/runtime~main.88d65856.js"
+  }
+]);
